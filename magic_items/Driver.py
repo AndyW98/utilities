40e2b0c8ddaa920c706c48fa0c25057e0665caa3
jquijot1 <jquijot1@gmail.com>
2022-04-02 16:54:08 -0400

    Add magic item dynamic economy

--- magic_items/Driver.py
+++ magic_items/Driver.py
@@ -9,22 +9,32 @@ Author(s):
 '''
 
 from MagicItems import *
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 if __name__ == '__main__':
     
     json_file = 'magic_items/magic_items.json'
     magic_items_file = 'magic_items/magic_items_price.json'
     magic_items_file2 = 'magic_items/magic_items_price2.json'
+    # d = {}
 
-    magic_items = MagicItems(json_file)
-    #print(magic_item_dict.items.keys())
+    # magic_items = MagicItems(json_file)
+    # magic_items.dumps(magic_items_file)
+    
+    magic_items = MagicItems(magic_items_file)
+    
+    magic_items.vary_prices(9)
+    magic_items.dumps(magic_items_file2)
 
-    with open(magic_items_file, 'w') as f:
-        json.dump(magic_items.items, f, indent = 4)
+    # d = {item_n: magic_items.get_price_history(item_n) for item_n in magic_items.items.keys()}
 
-    magic_items2 = MagicItems(magic_items_file)
+    # df = pd.DataFrame(data=d)
+    # df_subset = df[df.columns[range(8)]]
 
-    with open(magic_items_file2, 'w') as f:
-        json.dump(magic_items2.items, f, indent = 4)
-
+    # x = df_subset.plot()
+    # fig = x.get_figure()
+    # fig.savefig('plot.pdf')
+    
     print("Complete!")
--- magic_items/MagicItems.py
+++ magic_items/MagicItems.py
@@ -9,9 +9,15 @@ import json
 import random as r
 
 class MagicItems:
-    '''MagicItems class
+    '''
+    The MagicItems object contains the info about DnD 5e magic items
 
-
+    Args:
+        json_file (str): File in which magic item info is stored
+    
+    Attributes:
+        json_file (str): File in which magic item info is stored
+        items: Dictionary of all the magic items in 5e
     '''
     def __init__(self, json_file):
         self.json_filename = json_file
@@ -23,41 +29,90 @@ class MagicItems:
         # varies the prices based on a Triangle
         # distrubition with min = -1, max = 1, mode = 0
         if 'Price' not in self.items[list(self.items.keys())[0]]:
-            self.randomize_prices()
-        else:
-            self.vary_prices()
+            self.reset_prices()
 
-    def randomize_prices(self):
+    def get_price_history(self, item_n):
+        '''Gets price history of given item
         
-        price = 0
-        for item in self.items:
+        Args:
+            item_n (str): Name of magic item
+
+        Returns:
+            List of magic item's price history
+        '''
+        return self.items[item_n]['Price']
+
+    def reset_prices(self, average = False):
+        '''Initializes prices for each magic item'''
+        for item_n in self.items:
             #Removes HTML information
-            self.items[item].popitem()
+            if 'HTML' in self.items[list(self.items.keys())[0]]:
+                self.items[item_n].pop('HTML')
             
             # Prices based on XGE "Buying a Magic Item" Section        
-            if self.items[item]['Rarity'] == 'Common':
-                price = (r.randint(1, 6) + 1) * 10
-            elif self.items[item]['Rarity'] == 'Uncommon':
-                price = r.randint(1, 6) * 100 
-            elif self.items[item]['Rarity'] == 'Rare':
-                price = (r.randint(1, 10) + r.randint(1, 10)) * 1000
-            elif self.items[item]['Rarity'] == 'Very Rare':
-                price = (r.randint(1, 4) + 1) * 10000
-            elif self.items[item]['Rarity'] == 'Legendary':
-                price = (r.randint(1, 6) + r.randint(1, 6)) * 25000
-            elif self.items[item]['Rarity'] == '???':
-                price = 0
+            price = []
+            if average:
+                if self.items[item_n]['Rarity'] == 'Common':
+                    price.append(45)
+                elif self.items[item_n]['Rarity'] == 'Uncommon':
+                    price.append(350) 
+                elif self.items[item_n]['Rarity'] == 'Rare':
+                    price.append(11000)
+                elif self.items[item_n]['Rarity'] == 'Very Rare':
+                    price.append(35000)
+                elif self.items[item_n]['Rarity'] == 'Legendary':
+                    price.append(175000)
+                else:
+                    price.append(-1)
             else:
-                price = -1                
+                if self.items[item_n]['Rarity'] == 'Common':
+                    price.append((r.randint(1, 6) + 1) * 10)
+                elif self.items[item_n]['Rarity'] == 'Uncommon':
+                    price.append(r.randint(1, 6) * 100) 
+                elif self.items[item_n]['Rarity'] == 'Rare':
+                    price.append((r.randint(1, 10) + r.randint(1, 10)) * 1000)
+                elif self.items[item_n]['Rarity'] == 'Very Rare':
+                    price.append((r.randint(1, 4) + 1) * 10000)
+                elif self.items[item_n]['Rarity'] == 'Legendary':
+                    price.append((r.randint(1, 6) + r.randint(1, 6)) * 25000)
+                else:
+                    price.append(-1)
 
-            self.items[item].update({'Price': price})
+            self.items[item_n].update({'Price': price})
 
-    def vary_prices(self):
+    def vary_prices(self, months = 1):
+        '''Varies already established prices for each magic item per month
         
-        for item in self.items:
-            if self.items[item]['Price'] > 0:
-                new_price = round((r.triangular(-1, 1, 0) + 1) * self.items[item]['Price'])   
-                self.items[item].update({'Price': new_price})
-        
+        Args:
+            months (int): number of months to vary the prices
+        '''        
+        for n in range(months):
+            for item_n in self.items:
+                initial_price = self.items[item_n]['Price'][0]
+                current_price = self.items[item_n]['Price'][-1]
+                new_price = -1
+                if current_price >= 0:
+                    price_change_ratio = current_price / initial_price 
+                    if price_change_ratio > 0.5 or price_change_ratio < 1.5:
+                        new_price = max(round((r.triangular(0, 2, 1 / price_change_ratio)) * current_price), 1)   
+                    elif price_change_ratio >= 1.5:
+                        new_price = max(round((r.triangular(0, 2, 0)) * current_price), 1)                    
+                    elif price_change_ratio <= 0.5:
+                        new_price = max(round((r.triangular(0, 2, 2)) * current_price), 1)
+                    else:
+                        new_price = -5
+                        print('Economy broke.')
+                self.items[item_n]['Price'].append(new_price)
+                    
+                        
+
+    def dumps(self, filename):
+        '''Writes magic items into a given file
+
+        Args:
+            filename (str): Name of file to put data
+        ''' 
+        with open(filename, 'w') as file:
+            json.dump(self.items, file, indent = 4)
 
 
